14cbc8999504d0a5670c603e1d4b81a21700e26d
SubhamSengupta <sengupta335@gmail.com>
2017-08-10 01:26:18 +0530

    initial page setup

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log('Hello World!');
+const express = require('express');
+const path = require('path');
+var fs = require('fs');
+
+var app = express();
+var port = 3000;
+
+const publicPath = path.join(__dirname, './public');
+app.use(express.static(publicPath));
+
+
+app.listen(port, () =>{
+  console.log(`Server deployed on port ${port}`);
+});
