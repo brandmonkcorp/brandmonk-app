55f71e122ffff8014ff0bd2b221693b8827a9070
Sayak93 <chatterjee.sayak93@gmail.com>
2017-08-25 18:31:00 +0530

    vid player

--- public/js/home.js
+++ public/js/home.js
@@ -1,10 +1,31 @@
 $(document).ready( function () {
 
-  for (var i=1; i<=6; i++){
+  for (var i=1; i<=8; i++){
     var videoDiv = $(`<div id="video-${i}" class="video-divs"></div>`);
     $('#vid-container').append(videoDiv);
     $(`#video-${i}`).css('background-image', `url('./images/thumbnails/thumb${i}.png')`);
+    $(`#video-${i}`).click(function(){
+      $("video").attr("src",`https://1930455220.rsc.cdn77.org/bmonk/posVid-${i}.mp4`);
+      $("#overlay").show();
+
+      //$("#vid").attr("src", `https://1930455220.rsc.cdn77.org/bmonk/posVid-${i}.mp4`);
+      //$("#vidPlayer")[0].load();
+      //$("#vidPlayer").video(`<source src='https://1930455220.rsc.cdn77.org/bmonk/posVid-${i}.mp4' type="video/mp4"></source>`);
+    });
+    //  $("#vidPlayer").video(`src("https://1930455220.rsc.cdn77.org/bmonk/posVid-${i}mp4")`);
   }
+
+  $("#overlay").click(function(){
+    $("#overlay").hide();
+    $('#vidPlayer').get(0).pause();
+
+  });
+  $('#vidPlayer').click(false);
+
+
+
+
+
 });
 
 
