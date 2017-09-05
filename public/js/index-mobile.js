1ff4b9c1ef81102cb8a047fcd4894886c39c26fd
brandmonkcorp <brandmonkcorp@gmail.com>
2017-09-05 19:34:18 +0530

    mobile login and register

--- public/js/index-mobile.js
+++ public/js/index-mobile.js
@@ -1,16 +1,29 @@
 $(document).ready(function (){
-  $('#login-box-mobile').load('./pages_mobile/login-form');
+
 });
 
+
+$(document.body).on('click', '#mobile-login-button', function () {
+  $('#loading').css('visibility', 'visible');
+  $(document.body).load('./pages_mobile/login-form', function () {
+    $('#loading').css('visibility', 'hidden');
+  });
+});
+$(document.body).on('click', '#mobile-register-button', function () {
+  $('#loading').css('visibility', 'visible');
+  $(document.body).load('./pages_mobile/register-form', function () {
+    $('#loading').css('visibility', 'hidden');
+  });
+});
 $(document.body).on('click', '#register-mobile', function () {
   $('#loading').css('visibility', 'visible');
-  $('#login-box-mobile').load('./pages_mobile/register-form', function () {
+  $(document.body).load('./pages_mobile/register-form', function () {
     $('#loading').css('visibility', 'hidden');
   });
 });
 $(document.body).on('click', '#back-to-login', function () {
   $('#loading').css('visibility', 'visible');
-  $('#login-box-mobile').load('./pages_mobile/login-form', function () {
+  $(document.body).load('./pages_mobile/login-form', function () {
     $('#loading').css('visibility', 'hidden');
   });
 });
