e7fcbe7e1b0f544e4363ff973ffebb1037efe5e7
Shubhadeep Chowdhury <shhubhadeep.3g@gmail.com>
2017-09-09 02:11:08 +0530

    social page

---
+++ public/js/social.js
@@ -0,0 +1,12 @@
+$(document.body).on('click', '#social', function () {
+  location.href='social.html';
+});
+$(document.body).on('click', '#payments', function () {
+  location.href='payments.html';
+});
+$(document.body).on('click', '#offers', function () {
+  location.href='offers.html';
+});
+$(document.body).on('click', '#brandmonk', function () {
+  location.href='home.html';
+});
