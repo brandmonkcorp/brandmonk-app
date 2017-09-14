45e8e84f2c08262c621ce938dddecf1ad54c5b70
Shubhadeep Chowdhury <shhubhadeep.3g@gmail.com>
2017-09-15 01:24:42 +0530

    social update 15/09/2017

--- public/js/social.js
+++ public/js/social.js
@@ -69,11 +69,35 @@ function playNextFunc(){
   });
 
   for (var i=1; i<=8; i++){
-    var videoDiv = $(`<div id="Vid-${i}" class="videodivs"></div>`);
-    var title = $(`<div id="title-${i}" class="Tcontainer"></div>`);
-    $('#friend').append(videoDiv);
-    $(`#Vid-${i}`).append(title);
-    $(`#Vid-${i}`).css('background-image', `url('./images/thumbnails/AdSnippet- (${i}).png')`);
+    var friend = $(`<div id="Vid-${i}" class="f_image"></div>`);
+    var friend_name = $(`<div id="title-${i}" class="f_name"></div>`);
+    $('#friend').append(friend);
+    $(`#Vid-${i}`).append(friend_name);
+    $(`#Vid-${i}`).css('background-image', `url('../images/user_profile_female.jpg')`);
     $(`#title-${i}`).html(`Friend-${i}`);
   }
+  for (var j=1; j<=10; j++){
+    var post = $(`<div id="post_text-${j}" class="post"></div>`);
+    var post_u_name = $(`<div id="u_name-${j}" class="post_u_name"></div>`);
+    var like = $(`<div id="like-${j}" class="like"></div>`);
+    var comment = $(`<div id="comment-${j}" class="comment"></div>`);
+    $('#user_post').append(post);
+    $(`#post_text-${j}`).append(post_u_name);
+    $(`#post_text-${j}`).append(like);
+    $(`#post_text-${j}`).append(comment);
+    $(`#u_name-${j}`).html(`Post-${j}`);
+    $(`#like-${j}`).html(`Like`);
+    $(`#comment-${j}`).html(`comment`);
+
+    var ref_vid = $(`<div id="ref_vid-${j}" class="ref_vid"></div>`);
+    var ref_vid_name = $(`<div id="ref_vid_name-${j}" class="ref_vid_name"></div>`);
+    var share = $(`<div id="share-${j}" class="share"></div>`);
+    $('#user_post').append(ref_vid);
+    $(`#ref_vid-${j}`).append(ref_vid_name);
+    $(`#ref_vid-${j}`).append(share);
+    $(`#ref_vid-${j}`).css('background-image', `url('./images/thumbnails/AdSnippet- (${i}).png')`);
+    $(`#ref_vid_name-${j}`).html(`Add-${j}`);
+    $(`#share-${j}`).html(`share`);
+  }
+
 }
