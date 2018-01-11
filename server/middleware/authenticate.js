b26b06474712359798710aae26a53bd580aa34ed
brandmonkcorp <brandmonkcorp@gmail.com>
2018-01-11 12:19:36 +0530

    updated server for password recovery

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -8,12 +8,12 @@ var authenticate = (req, res, next) => {
     if(!user){
       return Promise.reject();
     }
-    console.log(userName, ' :connected status: authorized ip:',req.ip);
+    //console.log(userName, ' :connected status: authorized ip:',req.ip);
     req.user = user;
     req.token = token;
     next();
   }).catch((e) => {
-    console.log(userName, ' :connected status: unauthorized ip:',req.ip);
+    //console.log(userName, ' :connected status: unauthorized ip:',req.ip);
     res.status(401).send();
   });
 }
